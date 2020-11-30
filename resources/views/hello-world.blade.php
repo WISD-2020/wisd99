62b435cd4848eccd207d2347092d00aaa15a6f16
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-30 20:40:32 +0800

    livewire hello-world to HelloWorld

---
+++ resources/views/hello-world.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    @livewireStyles
+</head>
+<body>
+
+    @livewire('hello-world')
+
+    @livewireScripts
+
+</body>
+</html>
